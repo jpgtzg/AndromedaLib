30e6461c6b094dc011712171ed05f314e905f0e8
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2022-12-03 17:47:02 -0600

    Added math & conversions

--- src/main/java/com/team6647/math/Conversions.java
+++ src/main/java/com/team6647/math/Conversions.java
@@ -1,10 +1,117 @@
 package com.team6647.math;
 
-/*
+/**
  * Class that manages falcon conversions
  * Also manages conversions for other things
+ * 
+ * Copied over and modified from
+ * https://github.com/frc1678/C2022/blob/main/src/main/java/com/lib/math/Conversions.java
  */
 
 public class Conversions {
-    
+
+    /**
+     * Converts raw Falcon counts to degrees
+     * 
+     * @param counts    Falcon Counts
+     * @param gearRatio Gear Ratio between Falcon and Mechanism
+     * @return Degrees of Rotation of Mechanism
+     */
+    public static double falconToDegrees(double counts, double gearRatio) {
+        return counts * (360.0 / (gearRatio * 2048.0));
+    }
+
+    /**
+     * Converts degrees to raw Falcon counts
+     * 
+     * @param degrees   Degrees of rotation of Mechanism
+     * @param gearRatio Gear Ratio between Falcon and Mechanism
+     * @return Falcon Counts
+     */
+    public static double degreesToFalcon(double degrees, double gearRatio) {
+        double ticks = degrees / (360.0 / (gearRatio * 2048.0));
+        return ticks;
+    }
+
+    /**
+     * Converts raw Falcon counts to RPM
+     * 
+     * @param velocityCounts Falcon Velocity Counts
+     * @param gearRatio      Gear Ratio between Falcon and Mechanism (set to 1 for
+     *                       Falcon RPM)
+     * @return RPM of Mechanism
+     */
+    public static double falconToRPM(double velocityCounts, double gearRatio) {
+        double motorRPM = velocityCounts * (600.0 / 2048.0);
+        double mechRPM = motorRPM / gearRatio;
+        return mechRPM;
+    }
+
+    /**
+     * Converts RPM to raw Falcon counts
+     * 
+     * @param RPM       RPM of mechanism
+     * @param gearRatio Gear Ratio between Falcon and Mechanism (set to 1 for Falcon
+     *                  RPM)
+     * @return RPM of Mechanism
+     */
+    public static double RPMToFalcon(double RPM, double gearRatio) {
+        double motorRPM = RPM * gearRatio;
+        double sensorCounts = motorRPM * (2048.0 / 600.0);
+        return sensorCounts;
+    }
+
+    /**
+     * Converts raw Falcon counts to miles per second
+     * 
+     * @param velocitycounts Falcon Velocity Counts
+     * @param circumference  Circumference of Wheel
+     * @param gearRatio      Gear Ratio between Falcon and Mechanism (set to 1 for
+     *                       Falcon RPM)
+     * @return Falcon Velocity in miles per second
+     */
+    public static double falconToMPS(double velocitycounts, double circumference, double gearRatio) {
+        double wheelRPM = falconToRPM(velocitycounts, gearRatio);
+        double wheelMPS = (wheelRPM * circumference) / 60;
+        return wheelMPS;
+    }
+
+    /**
+     * Converts miles per second to raw Falcon counts
+     * 
+     * @param velocity      Velocity MPS
+     * @param circumference Circumference of Wheel
+     * @param gearRatio     Gear Ratio between Falcon and Mechanism (set to 1 for
+     *                      Falcon RPM)
+     * @return Falcon Velocity Counts
+     */
+    public static double MPSToFalcon(double velocity, double circumference, double gearRatio) {
+        double wheelRPM = ((velocity * 60) / circumference);
+        double wheelVelocity = RPMToFalcon(wheelRPM, gearRatio);
+        return wheelVelocity;
+    }
+
+    /**
+     * Converts raw Falcon counts to meters
+     * 
+     * @param velocitycounts Falcon Velocity Counts
+     * @param circumference  Circumference of Wheel in meters
+     * @return Falcon position in meters
+     */
+    public static double falconToMeters(double velocityCount, double circumference, double gearRatio) {
+        return velocityCount * ((circumference * Math.PI) / (gearRatio * 2048.0));
+    }
+
+    /**
+     * Converts from miles per second to meters per second
+     * 
+     * @param velocityCount Falcon Velocity Counts
+     * @param circumference Circumference of Wheel
+     * @param gearRatio     Gear Ratio between Falcon and Mechanism (set to 1 for
+     *                      Falcon RPM)
+     * @return Falcon velocity in Meters per Second
+     */
+    public static double MPSToMeterPerSecond(double velocityCount, double circumference, double gearRatio) {
+        return (falconToMPS(velocityCount, circumference, gearRatio) * 1609.344) / 3600;
+    }
 }
