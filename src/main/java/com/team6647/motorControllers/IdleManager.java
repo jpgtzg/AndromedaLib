26c5ff1ed223106246f1c81b5b244ce159b7156d
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2022-12-15 15:19:31 -0600

    Added return javadoc

--- src/main/java/com/team6647/motorControllers/IdleManager.java
+++ src/main/java/com/team6647/motorControllers/IdleManager.java
@@ -17,6 +17,8 @@ public class IdleManager {
      * Converts from GlobalIdle mode to corresponding NeutralMode
      * 
      * @param idleMode The GlobalIdleMode to convert
+     * 
+     * @return The corresponding NeutralMode
      */
     static NeutralMode idleToNeutral(GlobalIdleMode idleMode) {
         return NeutralMode.values()[idleMode.ordinal() + 1];
@@ -26,6 +28,8 @@ public class IdleManager {
      * Converts from GlobalIdle mode to corresponding IdleMode
      * 
      * @param idleMode The GlobalIdleMode to convert
+     * 
+     * @return The corresponding IdleMode
      */
     static IdleMode neutralToIdle(GlobalIdleMode idleMode) {
         return IdleMode.values()[idleMode.ordinal()];
