37fef33c002a3d657b1caafa02b1d57fac3437a2
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2022-12-30 21:22:45 -0600

    Added SparkMax Blinkin

--- src/main/java/com/team6647/motorControllers/SuperSparkMax.java
+++ src/main/java/com/team6647/motorControllers/SuperSparkMax.java
@@ -1,9 +1,12 @@
 package com.team6647.motorControllers;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
+import com.team6647.leds.Blinkin;
 import com.team6647.motorControllers.IdleManager.GlobalIdleMode;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /*
@@ -12,9 +15,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class SuperSparkMax extends CANSparkMax implements HyperMotorController {
 
+    Blinkin blinkin;
+
     private double currentLimit = 1;
 
     RelativeEncoder encoder = getEncoder();
+
+    REVLibError error;
 
     /**
      * Configures SuperSparkMax motor controller
@@ -32,6 +39,26 @@ public class SuperSparkMax extends CANSparkMax implements HyperMotorController {
         setInverted(isInverted);
         setSmartCurrentLimit(currentLimit);
         outputTelemetry();
+    }
+
+    /**
+     * Configures SuperSparkMax motor controller
+     * 
+     * @param id           ID of the motor controller
+     * @param type         Type of the motor controller
+     * @param idleMode     Idle mode of the motor controller
+     * @param inverted     Inverted state of the motor controller
+     * @param currentLimit Current limit of the motor controller in amps
+     */
+    public SuperSparkMax(int motorID, MotorType type, GlobalIdleMode idleMode, boolean isInverted, int currentLimit,
+            int blinkinPWMPort) {
+        super(motorID, type);
+        restoreFactoryDefaults();
+        setMode(idleMode);
+        setInverted(isInverted);
+        setSmartCurrentLimit(currentLimit);
+        outputTelemetry();
+        blinkin = new Blinkin(blinkinPWMPort);
     }
 
     @Override
@@ -91,7 +118,7 @@ public class SuperSparkMax extends CANSparkMax implements HyperMotorController {
     /**
      * Gets the position of the encoder
      * 
-     * @return the position of the encoder 
+     * @return the position of the encoder
      */
     public double getPosition() {
         return encoder.getPosition();
@@ -115,4 +142,77 @@ public class SuperSparkMax extends CANSparkMax implements HyperMotorController {
         encoder.setPosition(0);
     }
 
+    public void setErrorLED() {
+        error = getLastError();
+        switch (error) {
+            case kError:
+
+                break;
+            case kTimeout:
+
+                break;
+            case kNotImplemented:
+
+                break;
+
+            case kHALError:
+
+                break;
+            case kCantFindFirmware:
+
+                break;
+            case kFirmwareTooOld:
+
+                break;
+            case kFirmwareTooNew:
+
+                break;
+            case kParamInvalidID:
+
+                break;
+            case kParamMismatchType:
+
+                break;
+            case kParamAccessMode:
+
+                break;
+            case kParamInvalid:
+
+                break;
+            case kParamNotImplementedDeprecated:
+
+                break;
+            case kFollowConfigMismatch:
+
+                break;
+            case kInvalid:
+
+                break;
+            case kSetpointOutOfRange:
+
+                break;
+            case kUnknown:
+
+                break;
+            case kCANDisconnected:
+
+                break;
+            case kDuplicateCANId:
+
+                break;
+            case kInvalidCANId:
+
+                break;
+            case kSparkMaxDataPortAlreadyConfiguredDifferently:
+
+                break;
+            default:
+
+                break;
+        }
+
+        if(error != REVLibError.kOk) {
+            DriverStation.reportError("SparkMax Error: " + error + " , in motor " + getDeviceId(), false);
+        }
+    }
 }
