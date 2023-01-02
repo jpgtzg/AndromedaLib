1352c0526962ea6a97aaa736702aa0db1ef46db7
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-01-02 17:51:52 -0600

    Added Victor Error and Constructor

--- src/main/java/com/andromedalib/motorControllers/SuperVictor.java
+++ src/main/java/com/andromedalib/motorControllers/SuperVictor.java
@@ -1,17 +1,22 @@
 package com.andromedalib.motorControllers;
 
+import com.andromedalib.math.Conversions;
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/*
- * Wrapper for the WPI_VictorSPX class
- * That  implements the HyperMotorController interface
+/**
+ * Wrapper for the {@link WPI_VictorSPX} class
+ * That implements the HyperMotorController interface
  */
 public class SuperVictor extends WPI_VictorSPX implements HyperMotorController {
 
     private double currentLimit = 1;
+
+    ErrorCode error;
 
     /**
      * Configures SuperVictor motor controller
@@ -27,6 +32,20 @@ public class SuperVictor extends WPI_VictorSPX implements HyperMotorController {
         setMode(idleMode);
         setInverted(isInverted);
         // TODO SET CURRENT LIMIT
+    }
+
+    /**
+     * Configures SuperVictor motor controller
+     * 
+     * @param motorID    ID of the motor controller
+     * @param idleMode   Idle mode of the motor controller
+     * @param isInverted Inverted state of the motor controller
+     */
+    public SuperVictor(int motorID, GlobalIdleMode idleMode, boolean isInverted) {
+        super(motorID);
+        configFactoryDefault();
+        setMode(idleMode);
+        setInverted(isInverted);
     }
 
     @Override
@@ -66,34 +85,95 @@ public class SuperVictor extends WPI_VictorSPX implements HyperMotorController {
     }
 
     /**
-     * Gets the velocity of the motor controller
+     * Gets the position of the motor controller
      * 
-     * @param conversionFactor Conversion factor to convert encoder ticks to
-     *                         desired units
+     * @param circumference Circumference of the wheel in meters
+     * @param gearRatio     Gear ratio of the motor controller
      * 
-     * @return the velocity of the motor deisred units
+     * @return the position of the motor in meters
      */
-    public double getVelocity(double conversionFactor) {
-        //TODO FINISH
-        return getSelectedSensorVelocity() * conversionFactor;
+    public double getPosition(double circumference, double gearRatio) {
+        return Conversions.falconToMeters(getSelectedSensorPosition(), circumference, gearRatio);
     }
 
     /**
-     * Gets the position of the motor controller
+     * Gets the velocity of the motor controller
      * 
-     * @param conversionFactor Conversion factor to convert encoder ticks to
-     *                         desired units
+     * @param circumference Circumference of the wheel in meters
+     * @param gearRatio     Gear ratio of the motor controller
      * 
-     * @return the position of the motor in desired units
+     * @return the velocity of the motor in meters per second
      */
-    public double getPosition(double conversionFactor) {
-        //TODO FINISH
-        return getSelectedSensorPosition() * conversionFactor;
+    public double getVelocity(double circumference, double gearRatio) {
+        return Conversions.MPSToMeterPerSecond(getSelectedSensorVelocity(), circumference, gearRatio);
     }
 
     @Override
-    public void resetEncoder(){
-        //TODO COMPLETE
+    public void resetEncoder() {
+        // TODO COMPLETE
+    }
+
+    /**
+     * Sets the LED to the {@link ErrorCode} error
+     * in the motor controller. You must start running this method
+     * in your Subsystem's periodic method.
+     */
+    @Override
+    public void setErrorLED() {
+        error = getLastError();
+        if (error != ErrorCode.OK) {
+            DriverStation.reportError("VictorSPX Error: " + error + " , in motor " + getDeviceID(), false);
+            switch (error) {
+                case CAN_MSG_STALE:
+
+                    break;
+                case CAN_TX_FULL:
+
+                    break;
+                case TxFailed:
+
+                    break;
+
+                case InvalidParamValue:
+
+                    break;
+                case CAN_INVALID_PARAM:
+
+                    break;
+                case RxTimeout:
+
+                    break;
+                case CAN_MSG_NOT_FOUND:
+
+                    break;
+                case TxTimeout:
+
+                    break;
+                case CAN_NO_MORE_TX_JOBS:
+
+                    break;
+                case UnexpectedArbId:
+
+                    break;
+                case CAN_NO_SESSIONS_AVAIL:
+
+                    break;
+                case BufferFull:
+
+                    break;
+                case CAN_OVERFLOW:
+
+                    break;
+                case SensorNotPresent:
+
+                    break;
+                case FirmwareTooOld:
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 }
