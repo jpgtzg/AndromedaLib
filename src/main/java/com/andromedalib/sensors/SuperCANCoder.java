8956a04491d7819cc5c3c352c873fde799450087
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-24 12:40:28 -0600

    Created SuperCANCoder wrapper

---
+++ src/main/java/com/andromedalib/sensors/SuperCANCoder.java
@@ -0,0 +1,44 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
+package com.andromedalib.sensors;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class SuperCANCoder extends CANCoder {
+
+    /**
+     * SuperCANCoder constructor
+     * 
+     * @param deviceID      CANCoder DeviceID
+     * @param configuration Configuration
+     */
+    public SuperCANCoder(int deviceID, CANCoderConfiguration configuration) {
+        super(deviceID);
+
+        this.configFactoryDefault();
+        this.configAllSettings(configuration);
+    }
+
+    /**
+     * Gets the absolute degrees position
+     * 
+     * @return Absolute position in degrees
+     */
+    public double getDegrees() {
+        return getRotation().getDegrees();
+    }
+
+    /**
+     * Gets the absolute {@link Rotation2d} positions
+     * 
+     * @return Absolute positions
+     */
+    public Rotation2d getRotation() {
+        return Rotation2d.fromDegrees(getAbsolutePosition());
+    }
+}
