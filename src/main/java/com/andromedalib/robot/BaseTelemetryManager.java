c962d9768c9138ae693152751d2574690641809a
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-05 20:46:14 -0600

    Fixed TelemetryManager redundancies

---
+++ src/main/java/com/andromedalib/robot/BaseTelemetryManager.java
@@ -0,0 +1,18 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
+package com.andromedalib.robot;
+
+/**
+ * Base Telemetry manager class. Inherit this class and override its methods to
+ * suit your needs
+ * It is automatically updated by the {@link SuperRobot} class
+ */
+public class BaseTelemetryManager {
+    public void initTelemetry() {
+    }
+
+    public void updateTelemetry() {
+    }
+}
