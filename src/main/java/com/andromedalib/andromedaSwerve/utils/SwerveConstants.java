170206f664825cf4e8abc66c6c8b6a757f353044
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-19 20:23:38 -0600

    Added comments

--- src/main/java/com/andromedalib/andromedaSwerve/utils/SwerveConstants.java
+++ src/main/java/com/andromedalib/andromedaSwerve/utils/SwerveConstants.java
@@ -7,36 +7,46 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
 public class SwerveConstants {
-    /* Represents Swerve-wide constants */
+        /* Represents Swerve-wide constants */
 
-    public static final double deadband = 0.1;
-    /* public static final AndromedaProfileConfig andromedaProfile = AndromedaProfileConfig
-            .getConfig(AndromedaProfiles.ANDROMEDA_CONFIG); */
+        public static final double deadband = 0.1;
+        /*
+         * public static final AndromedaProfileConfig andromedaProfile =
+         * AndromedaProfileConfig
+         * .getConfig(AndromedaProfiles.ANDROMEDA_CONFIG);
+         */
 
-    /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(18.5); 
-    public static final double wheelBase = Units.inchesToMeters(18.5);
-    public static final double wheelCircumference = AndromedaSwerve.andromedaProfile.wheelCircumference;
-    public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
-            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
-            new Translation2d(wheelBase / 2.0, trackWidth / 2.0));
+        /* Drivetrain Constants */
+        public static final double trackWidth = Units.inchesToMeters(18.5);
+        public static final double wheelBase = Units.inchesToMeters(18.5);
+        public static final double wheelCircumference = AndromedaSwerve.andromedaProfile.wheelCircumference;
 
-    /*
-     * Drive Motor Characterization Values
-     * Divide SYSID values by 12 to convert from volts to percent output for CTRE
-     */
-    public static final double driveKS = (0.32 / 12); // TODO TUNE
-    public static final double driveKV = (1.51 / 12);
-    public static final double driveKA = (0.27 / 12);
+        /*
+         * This has to do with the robot-centric coordinate system in WPILib
+         * The convention is that +x is out front from the robot’s perspective and +y is
+         * out left of the robot (you can verify this with the right-hand rule, +z is up
+         * so counter-clockwise rotation is positive, which checks out).
+         */
+        public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+                        new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+                        new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0),
+                        new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+                        new Translation2d(wheelBase / 2.0, trackWidth / 2.0));
 
-    /* Swerve Profiling Values */
-    /** Meters per Second */
-    public static final double maxSpeed = 5.4864;
-    public static final double maxAcceleration = maxSpeed * 0.85;
-    /** Radians per Second */
-    public static final double maxAngularVelocity = 11.5;
-    public static final double maxAngularAcceleration = 3.5;
+        /*
+         * Drive Motor Characterization Values
+         * Divide SYSID values by 12 to convert from volts to percent output for CTRE
+         */
+        public static final double driveKS = (0.32 / 12); // TODO TUNE
+        public static final double driveKV = (1.51 / 12);
+        public static final double driveKA = (0.27 / 12);
+
+        /* Swerve Profiling Values */
+        /** Meters per Second */
+        public static final double maxSpeed = 5.4864;
+        public static final double maxAcceleration = maxSpeed * 0.85;
+        /** Radians per Second */
+        public static final double maxAngularVelocity = 11.5;
+        public static final double maxAngularAcceleration = 3.5;
 
 }
