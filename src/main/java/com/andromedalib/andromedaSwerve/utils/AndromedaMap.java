7fae72b061a04885e2bf88b2837c0af7b5dc9d77
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-20 15:57:15 -0700

    Updated Legacy module offsets

--- src/main/java/com/andromedalib/andromedaSwerve/utils/AndromedaMap.java
+++ src/main/java/com/andromedalib/andromedaSwerve/utils/AndromedaMap.java
@@ -19,28 +19,28 @@ public class AndromedaMap {
         public static final int steerID1 = 1;
         public static final int driveID1 = 2;
         public static final int absEncoder1 = 3;
-        public static final Rotation2d mod1Angleoffset = Rotation2d.fromDegrees(179.033);
+        public static final Rotation2d mod1Angleoffset = Rotation2d.fromDegrees(179.384);
         public static final AndromedaModuleConstants mod1Const = new AndromedaModuleConstants(steerID1, driveID1,
                         absEncoder1, mod1Angleoffset);
 
         public static final int steerID2 = 4;
         public static final int driveID2 = 5;
         public static final int absEncoder2 = 6;
-        public static final Rotation2d mod2Angleoffset = Rotation2d.fromDegrees(233.613);
+        public static final Rotation2d mod2Angleoffset = Rotation2d.fromDegrees(233.876);
         public static final AndromedaModuleConstants mod2Const = new AndromedaModuleConstants(steerID2, driveID2,
                         absEncoder2, mod2Angleoffset);
 
         public static final int steerID3 = 7;
         public static final int drivelID3 = 8;
         public static final int absEncoder3 = 9;
-        public static final Rotation2d mod3Angleoffset = Rotation2d.fromDegrees(163.386);
+        public static final Rotation2d mod3Angleoffset = Rotation2d.fromDegrees(162.333);
         public static final AndromedaModuleConstants mod3Const = new AndromedaModuleConstants(steerID3, drivelID3,
                         absEncoder3, mod3Angleoffset);
 
         public static final int steerID4 = 10;
         public static final int driveID4 = 11;
         public static final int absEncoder4 = 12;
-        public static final Rotation2d mod4Angleoffset = Rotation2d.fromDegrees(23.994);
+        public static final Rotation2d mod4Angleoffset = Rotation2d.fromDegrees(23.466);
         public static final AndromedaModuleConstants mod4Const = new AndromedaModuleConstants(steerID4, driveID4,
                         absEncoder4, mod4Angleoffset);
 }
