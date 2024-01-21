d11f4df75a17829d40d4d0960ba4606a06ee323d
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-20 18:40:57 -0600

    Removed unused import

--- src/main/java/com/andromedalib/andromedaSwerve/subsystems/AndromedaSwerve.java
+++ src/main/java/com/andromedalib/andromedaSwerve/subsystems/AndromedaSwerve.java
@@ -22,7 +22,6 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AndromedaSwerve extends SubsystemBase {
